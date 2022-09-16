2243507289a885238112260245858daf4da5a3ea
junioralcant <junioralcant111@gmail.com>
2022-09-16 17:42:42 -0300

    Config expo notification

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
 import { StatusBar } from 'react-native';
 
 import {
@@ -7,12 +9,16 @@ import {
   Inter_700Bold,
   Inter_900Black,
 } from '@expo-google-fonts/inter';
+import { Subscription } from 'expo-modules-core';
 
 import { Home } from './src/screens/Home/Index';
 import { Loading } from './src/components/Loading';
 import { Background } from './src/components/Background';
 
 import { Routes } from './src/routes';
+
+import './src/services/notificationConfigs';
+import { getPushNotificatonToken } from './src/services/getPushNotificatonToken';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,6 +27,43 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificatonToken();
+  }, []);
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener(
+        (notifications) => {
+          console.log(notifications);
+        }
+      );
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener(
+        (response) => {
+          console.log(response);
+        }
+      );
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
 
   return (
     <Background>
